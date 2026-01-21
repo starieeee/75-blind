9f1f189cb18ba3f0deb8d01d42ea57c5470767a9
Natepy Vann <162285803+starieeee@users.noreply.github.com>
2026-01-21 16:39:28 -0600

    commit

---
+++ climbingStairs.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n <= 2:
+            return n
+        
+        prev2 = 1
+        prev1 = 2
+        for i in range(3, n + 1):
+            current = prev1 + prev2
+            prev2 = prev1
+            prev1 = current
+        return prev1
